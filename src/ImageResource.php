f2c5bbef255f6838d50b70558b076c2bb477ab39
Tino Ehrich <tino@bigpun.me>
2017-10-09 18:10:20 +0200

    throw exception if we cannot load image

--- src/ImageResource.php
+++ src/ImageResource.php
@@ -9,11 +9,18 @@ class ImageResource extends Resource
 {
     /**
      * @param mixed $source
+     *
+     * @throws \Exception
      */
     public function __construct($source)
     {
         $this->resource = imagecreatefromstring(
             file_get_contents($source)
         );
+
+        if ($this->resource === false)
+        {
+            throw new \Exception('Could not fetch "' . $source . '"');
+        }
     }
 }
